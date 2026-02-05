26adb126023c0568a28b784854e475d31bdf0ade
Godson <gnqnpn@gmail.com>
2026-02-05 14:27:29 +0100

    some changes

--- libs/shared/src/utils/otpgenerator.ts
+++ libs/shared/src/utils/otpgenerator.ts
@@ -1,8 +1,7 @@
 import { timeZoneMoment } from './others';
 
 export const GenerateOtp = () => {
-  // const otp = '1234';
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  const otp = Math.floor(1000 + Math.random() * 9000).toString();
   let expiry = timeZoneMoment().add(10, 'm').toDate();
 
   return { otp, expiry };
